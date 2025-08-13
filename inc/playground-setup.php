95177709533f491d8ab3fef9f56fa6791c7e4430
George Stephanis <daljo628@gmail.com>
2025-08-13 11:34:02 -0400

    Attempt to run it from github instead for cors reasons.

--- inc/playground-setup.php
+++ inc/playground-setup.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once( ABSPATH . 'wp-admin/includes/media.php' );
-require_once( ABSPATH . 'wp-admin/includes/file.php' );
-require_once( ABSPATH . 'wp-admin/includes/image.php' );
+require_once( \ABSPATH . 'wp-admin/includes/media.php' );
+require_once( \ABSPATH . 'wp-admin/includes/file.php' );
+require_once( \ABSPATH . 'wp-admin/includes/image.php' );
 
 $site_logo_id = \media_sideload_image(
-    'https://wapu.us/wp-content/uploads/2016/07/original_wapuu.png',
+    'https://raw.githubusercontent.com/georgestephanis/twombly/refs/heads/main/inc/original_wapuu.png',
     null,
     'Wapuu!',
     'id'
