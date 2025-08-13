1ccce205e593caebdcd0e0ced20aefe806b57caa
George Stephanis <daljo628@gmail.com>
2025-08-13 11:50:30 -0400

    Fix whitespace, also redirect back to gh url.

--- _playground/setup.php
+++ _playground/setup.php
@@ -4,17 +4,17 @@
  * Clear the database for the subsequent WXR import.
  */
 $posts = \get_posts(
-    array(
-        'numberposts' => -1,
-        'post_status' => 'any',
-        'post_type'   => array(
-            'post',
-            'page',
-        ),
-    )
+	array(
+		'numberposts' => -1,
+		'post_status' => 'any',
+		'post_type'   => array(
+			'post',
+			'page',
+		),
+	)
 );
 foreach ( $posts as $post) {
-    \wp_delete_post( $post->ID, true );
+	\wp_delete_post( $post->ID, true );
 }
 
 /**
@@ -25,12 +25,12 @@ require_once( \ABSPATH . 'wp-admin/includes/file.php' );
 require_once( \ABSPATH . 'wp-admin/includes/image.php' );
 
 $site_icon_id = \media_sideload_image(
-    get_theme_file_uri( '_playground/site_icon.png' ),
-    null,
-    'Site Icon',
-    'id'
+	'https://raw.githubusercontent.com/georgestephanis/twombly/main/_playground/site_icon.png',
+	null,
+	'Site Icon',
+	'id'
 );
 
 if ( ! \is_wp_error( $site_icon_id ) ) {
-    \update_option( 'site_icon', $site_icon_id );
+	\update_option( 'site_icon', $site_icon_id );
 }
