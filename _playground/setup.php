78031ca8bfefba2d6764baed19c5d43afbf7377f
George Stephanis <daljo628@gmail.com>
2025-08-19 16:38:15 -0400

    phpcs linting.

--- _playground/setup.php
+++ _playground/setup.php
@@ -1,9 +1,14 @@
 <?php
+/**
+ * Some setup scripts to run when initializing a Playground environment.
+ *
+ * @package Iconick/Twombly
+ */
 
 /**
  * Clear the database for the subsequent WXR import.
  */
-$posts = \get_posts(
+$twombly_posts = \get_posts(
 	array(
 		'numberposts' => -1,
 		'post_status' => 'any',
@@ -13,24 +18,24 @@ $posts = \get_posts(
 		),
 	)
 );
-foreach ( $posts as $post) {
-	\wp_delete_post( $post->ID, true );
+foreach ( $twombly_posts as $twombly_post ) {
+	\wp_delete_post( $twombly_post->ID, true );
 }
 
 /**
  * Load in a Site Icon.
  */
-require_once( \ABSPATH . 'wp-admin/includes/media.php' );
-require_once( \ABSPATH . 'wp-admin/includes/file.php' );
-require_once( \ABSPATH . 'wp-admin/includes/image.php' );
+require_once \ABSPATH . 'wp-admin/includes/media.php';
+require_once \ABSPATH . 'wp-admin/includes/file.php';
+require_once \ABSPATH . 'wp-admin/includes/image.php';
 
-$site_icon_id = \media_sideload_image(
+$twombly_site_icon_id = \media_sideload_image(
 	'https://raw.githubusercontent.com/IconickThemes/twombly/main/_playground/site_icon.png',
 	null,
 	'Site Icon',
 	'id'
 );
 
-if ( ! \is_wp_error( $site_icon_id ) ) {
-	\update_option( 'site_icon', $site_icon_id );
+if ( ! \is_wp_error( $twombly_site_icon_id ) ) {
+	\update_option( 'site_icon', $twombly_site_icon_id );
 }
