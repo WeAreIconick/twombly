3090f419184221592abbd58f8fb7c5b459d9c034
George Stephanis <daljo628@gmail.com>
2025-08-19 14:20:31 -0400

    Add a Homepage pattern.

---
+++ patterns/homepage.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Title: Homepage Layout
+ * Slug: twombly/homepage
+ * Description: A format for the easy creation of a homepage.
+ * Keywords: homepage, twombly
+ * Post Types: page
+ * Inserter: yes
+ */
+?>
+
+<!-- wp:group {"metadata":{"name":"Home Page Layout","categories":[],"patternName":"core/block/2040"},"style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"0","margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}},"typography":{"lineHeight":"1.1"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);line-height:1.1"><!-- wp:heading {"textAlign":"center","level":1} -->
+<h1 class="wp-block-heading has-text-align-center">I'm Carl.</h1>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"center","level":1} -->
+<h1 class="wp-block-heading has-text-align-center">A Digital Nomad.</h1>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"center","level":6,"style":{"elements":{"link":{"color":{"text":"var:preset|color|text"}}},"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"textColor":"text"} -->
+<h6 class="wp-block-heading has-text-align-center has-text-color has-link-color" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--50)">I love designing meaningful experiences and teaching design.</h6>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"is-style-twombly-card","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-twombly-card"><!-- wp:cover {"url":"https://iconick.wp.local/wp-content/uploads/2025/08/00006-640614319-1024x701.jpeg","id":1956,"alt":"[img-1] The main item I see in this image is a man standing with his hands on his hips and wearing a baseball cap. He appears to be looking off into the distance.","dimRatio":50,"isUserOverlayColor":true,"align":"full","style":{"color":[]},"layout":{"type":"default"}} -->
+<div class="wp-block-cover alignfull"><img class="wp-block-cover__image-background wp-image-1956" alt="[img-1] The main item I see in this image is a man standing with his hands on his hips and wearing a baseball cap. He appears to be looking off into the distance." src="https://iconick.wp.local/wp-content/uploads/2025/08/00006-640614319-1024x701.jpeg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size"></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":5} -->
+<h5 class="wp-block-heading">Clonify Course +</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"fontSize":"xs"} -->
+<p class="has-xs-font-size">A full-fledged Framer course.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"style":{"layout":{"selfStretch":"fill","flexSize":null}}} -->
+<div aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><strong>Enroll now</strong></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:group {"className":"is-style-twombly-card","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-twombly-card"><!-- wp:heading {"level":5} -->
+<h5 class="wp-block-heading">Framer Award</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"fontSize":"xs"} -->
+<p class="has-xs-font-size">Kozmo was voted SOTM by Framer.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"66.66%"} -->
+<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:group {"className":"is-style-twombly-card","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-twombly-card"><!-- wp:heading {"level":5} -->
+<h5 class="wp-block-heading">Resources</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"fontSize":"xs"} -->
+<p class="has-xs-font-size">Browse the best tools</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:embed {"url":"https://www.youtube.com/watch?v=CmzKQ3PSrow","type":"video","providerNameSlug":"youtube","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
+<figure class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
+https://www.youtube.com/watch?v=CmzKQ3PSrow
+</div></figure>
+<!-- /wp:embed --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:group {"className":"is-style-twombly-card","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-twombly-card"><!-- wp:heading {"level":5} -->
+<h5 class="wp-block-heading">Framer Templates</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"fontSize":"xs"} -->
+<p class="has-xs-font-size">Never start from scratch, build stunning sites with top-notch Framer templates.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
